d633be2bdf9c840f240b118d20c6a1ce755eba4e
Alexander Smolyanskiy <asmoly@outlook.com>
2023-05-17 18:20:34 -0700

    added new post processing algorithm

--- code/model_tester_3.py
+++ code/model_tester_3.py
@@ -12,6 +12,15 @@ import numpy as np
 from PIL import Image
 import person_finder_AI
 
+def rectangle_overlap(ax, ay, ax1, ay1, bx, by, bx1, by1):
+    dx = min(ax1, bx1) - max(ax, bx)
+    dy = min(ay1, by1) - max(ay, by)
+
+    if dx >= 0 and dy >= 0:
+        return dx*dy
+    else:
+        return 0
+
 cam = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
@@ -30,7 +39,7 @@ while True:
 
     with torch.no_grad():
         modelOutput = model(image.to(device)).cpu()
-        modelOutput[0] = nn.Threshold(0.9, 0.0)(modelOutput[0])
+        modelOutput[0] = nn.Threshold(0.98, 0.0)(modelOutput[0])
 
         # Seperating mask from model output and resising it
         outputMask = modelOutput[0:1]
@@ -55,32 +64,54 @@ while True:
 
         resultMask = (modelOutput[0]*255).cpu().detach().numpy().astype(np.uint8)
         mask = np.zeros(image.shape, dtype=np.uint8)
-        thresh = cv2.threshold(resultMask, 255, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        thresh = cv2.threshold(resultMask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
         blobs = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         blobs = blobs[0] if len(blobs) == 2 else blobs[1]
 
+        bboxes = []
         for blob in blobs:
-            if len(blob) > 40:
-                # blob = np.array(blob)
-                # blob = blob.reshape((blob.shape[0], 2))
+            if len(blob) > 10:
+                blob = np.array(blob)
+                blob = blob.reshape((blob.shape[0], 2))
 
-                # averageBbox = np.array([0.0, 0.0, 0.0, 0.0])
+                averageBbox = np.array([0.0, 0.0, 0.0, 0.0])
 
-                # for i in range (0, len(blob)):
-                #     currentBbox = modelOutput[1:, blob[i][1], blob[i][0]]
+                for i in range (0, len(blob)):
+                    currentBbox = modelOutput[1:, blob[i][1], blob[i][0]]
 
-                #     averageBbox[0] += blob[i][0]/256*1024 + currentBbox[0]*1024
-                #     averageBbox[1] += blob[i][1]/256*1024 + currentBbox[1]*1024
-                #     averageBbox[2] += currentBbox[2]*1024
-                #     averageBbox[3] += currentBbox[3]*1024
+                    averageBbox[0] += blob[i][0]/256*1024 + currentBbox[0]*1024
+                    averageBbox[1] += blob[i][1]/256*1024 + currentBbox[1]*1024
+                    averageBbox[2] += currentBbox[2]*1024
+                    averageBbox[3] += currentBbox[3]*1024
 
-                # averageBbox = (averageBbox/len(blob)).astype(int)
+                averageBbox = (averageBbox/len(blob)).astype(int)
+                bboxes.append(averageBbox)
 
-                # outputImage = cv2.rectangle(outputImage, (averageBbox[0], averageBbox[1]), (averageBbox[0] + averageBbox[2], averageBbox[1] + averageBbox[3]), color=(0, 0, 255), thickness=2)
-                
-                cv2.circle(outputImage, (int(blob[0][0][0]/256*1024), int(blob[0][0][1]/256*1024)), 10, color=(0, 0, 255), thickness=10)
+                #outputImage = cv2.rectangle(outputImage, (averageBbox[0], averageBbox[1]), (averageBbox[0] + averageBbox[2], averageBbox[1] + averageBbox[3]), color=(0, 0, 255), thickness=2)
 
+        new_bboxes = []
+        for bbox in bboxes:
+            x = bbox[0]
+            y = bbox[1]
+            x1 = bbox[0] + bbox[2]
+            y1 = bbox[1] + bbox[3]
+            
+            if len(new_bboxes) == 0:
+                new_bboxes.append([x, y, x1, y1])
+            else:
+                overlaping = False
+                for i in range (0, len(new_bboxes)):
+                    overlaping_area = rectangle_overlap(x, y, x1, y1, new_bboxes[i][0], new_bboxes[i][1], new_bboxes[i][2], new_bboxes[i][3])
+
+                    if overlaping_area > 50:
+                        overlaping = True
+
+                if overlaping == False:
+                    new_bboxes.append([x, y, x1, y1])
+
+        for bbox in new_bboxes:
+            outputImage = cv2.rectangle(outputImage, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color=(0, 0, 255), thickness=2)
 
     cv2.imshow("video", outputImage)
     cv2.waitKey(1)
